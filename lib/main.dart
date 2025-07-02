2a838ead56cd5ec62d39f58ac5a58738f42fca8c
Aleksandra Kovaleva <ak.nova8yte@gmail.com>
2025-07-02 16:55:30 +0500

    Init commit lol

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(app)
+  // runZonedGuarded(body, onError) runZonedGuarded is actually a dope thing.
+  // It provides you with single point of logging for Errors. 
+}
+
+class InterviewChaet extends StatelessWidget {
+
+} 
+
+class CheatHome
+
+
+class _CheatHomeState
+
+
